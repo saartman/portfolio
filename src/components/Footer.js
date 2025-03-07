8d6adf5f19e94b594eca6ad6301cd4fa3a6e7def
Saartman <fedorov.gleb3@gmail.com>
2025-03-08 02:12:45 +0300

    Описание изменений 2

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+      <footer style={{ backgroundColor: "#282c34", padding: "10px", color: "#fff", textAlign: "center" }}>
+        <p>&copy; 2025 My Portfolio. All Rights Reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
