e93195ccb9a8bce200eab57878408b76d580367a
saartman <fedorov.gleb3@gmail.com>
2025-06-18 10:30:10 +0300

    Описание изменений

--- src/components/elements/btn_colored.tsx
+++ src/components/elements/btn_colored.tsx
@@ -3,11 +3,12 @@ import React from "react";
 
 interface BtnColoredProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  textClassName?: string;
 }
 
 export const BtnColored = React.forwardRef<HTMLButtonElement, BtnColoredProps>(
   (
-    { children, className = '', type = 'button', disabled = false, ...props },
+    { children, className = '', type = 'button', disabled = false, textClassName = '', ...props },
     ref
   ) => {
     return (
@@ -16,20 +17,34 @@ export const BtnColored = React.forwardRef<HTMLButtonElement, BtnColoredProps>(
         type={type}
         disabled={disabled}
         className={`
-          inline-flex items-center justify-center
-          w-auto
-          px-6 py-2 rounded-lg font-semibold text-white
-          bg-blue-600
-          hover:bg-blue-700
-          active:bg-blue-800
-          focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2
-          disabled:bg-gray-400 disabled:cursor-not-allowed
+          group
+          inline-flex flex-col justify-center items-start w-fit h-fit -m-4
+          rounded-lg font-semibold
+          bg-transparent
+          focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2
+          disabled:cursor-not-allowed
           transition-colors duration-200
           ${className}
         `}
         {...props}
       >
-        {children}
+        <div className="self-stretch pl-5 pr-3.5 flex flex-col justify-start items-start gap-2.5">
+        <div
+          className={`
+            self-stretch h-6 p-2.5 bg-pink-600/10 rounded-[90px]
+            bg-transparent
+            group-hover:bg-pink-600/20
+            group-active:bg-pink-600/10
+            disabled:bg-transparent
+            transition-colors duration-200
+            pointer-events-none
+          `}
+          aria-hidden="true"
+        />
+        </div>
+        <span className={`justify-start text-black text-4xl font-bold font-['Manrope'] ${textClassName}`}>
+          {children}
+        </span>
       </button>
     );
   }
